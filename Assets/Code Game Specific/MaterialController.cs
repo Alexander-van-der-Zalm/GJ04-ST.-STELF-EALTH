7c3ad32ff0abbd5a22cc65b223dafb31db12d499
Alexander van der Zalm <alexandervdzalm@gmail.com>
2014-09-14 00:46:02 +0200

    FogTint
    The further away you are the dark you are.

--- Assets/Code Game Specific/MaterialController.cs
+++ Assets/Code Game Specific/MaterialController.cs
@@ -4,16 +4,27 @@ using System.Collections;
 public class MaterialController : MonoBehaviour 
 {
     public Color SkinTint, ClothingTint;
+    public Color LightTint;
+    //public Color FogTint;
 
+    private Transform tr;
+
+   // public Material mat;
+    SpriteRenderer renderer;
 	// Use this for initialization
 	void Start () 
     {
-	
+        tr = transform;
+        // mat = new Material(GetComponent<SpriteRenderer>().material);
+        renderer = GetComponent<SpriteRenderer>();
+        //.renderer..color =
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-	
+        renderer.color = FogTintController.GetFogTint(tr.position.y); 
+
 	}
 }
