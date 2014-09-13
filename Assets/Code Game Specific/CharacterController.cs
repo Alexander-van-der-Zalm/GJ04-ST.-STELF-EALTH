d9084530bf0904bcad1971549f2c980ea5df3ae0
Alexander van der Zalm <alexandervdzalm@gmail.com>
2014-09-13 17:17:29 +0200

    Basic scene: char controller Flip

---
+++ Assets/Code Game Specific/CharacterController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour 
+{
+    private bool facingLeft = true;
+
+    private Transform tr;
+    private Rigidbody2D rb;
+
+    private Vector2 movementInput = new Vector2();
+    // Use this for initialization
+	void Start () 
+    {
+        tr = transform;
+        rb = rigidbody2D;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        
+	}
+
+    public void SetInput(float horizontalInput, float verticalInput)
+    {
+        movementInput.x = horizontalInput;
+        movementInput.y = verticalInput;
+    }
+
+    private void HandleZDepth()
+    {
+        Vector3 pos = tr.position;
+        pos.z = pos.y;
+        tr.position = pos;
+    }
+
+    private void Flip()
+    {
+        facingLeft = !facingLeft;
+        Vector3 localScale = tr.localScale;
+        localScale.x = -localScale.x;
+        tr.localScale = localScale;
+    }
+}
