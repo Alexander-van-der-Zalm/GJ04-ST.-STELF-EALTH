4720b9698b786e787c3e587483a52ad9b1c242b9
Alexander van der Zalm <alexandervdzalm@gmail.com>
2014-09-13 17:48:33 +0200

    Movement: two makes a crowd

--- Assets/Code Game Specific/CharacterController.cs
+++ Assets/Code Game Specific/CharacterController.cs
@@ -1,31 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class CharacterController : MonoBehaviour 
 {
+    public float MaximumVelocity = 10;
+
     private bool facingLeft = true;
 
     private Transform tr;
     private Rigidbody2D rb;
 
-    private Vector2 movementInput = new Vector2();
+    public Vector2 movementInput = new Vector2();
+
     // Use this for initialization
 	void Start () 
     {
         tr = transform;
         rb = rigidbody2D;
+        rb.gravityScale = 0;
+        rb.fixedAngle = true;
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-        
+        rb.velocity = movementInput * MaximumVelocity;
+        HandleZDepth();
 	}
 
     public void SetInput(float horizontalInput, float verticalInput)
     {
         movementInput.x = horizontalInput;
         movementInput.y = verticalInput;
+        movementInput.Normalize();
     }
 
     private void HandleZDepth()
---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerControl : MonoBehaviour 
+{
+    public enum PlayerActions
+    {
+        PickPocket
+
+    }
+    
+    public ControlScheme ControlScheme;
+    
+   
+    public Vector2 Input;
+
+    private CharacterController cc;
+    
+	// Use this for initialization
+	void Start () 
+    {
+        cc = gameObject.GetComponent<CharacterController>();
+
+        ControlScheme = ControlScheme.CreateScheme<PlayerActions>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        ControlScheme.Update();
+        cc.SetInput(ControlScheme.Horizontal.Value(), ControlScheme.Vertical.Value());
+        Input = new Vector2(ControlScheme.Horizontal.Value(), ControlScheme.Vertical.Value());
+	}
+}
