3bf69c1804b275bf15aba6d23105f515e7bf4025
Alexander van der Zalm <alexandervdzalm@gmail.com>
2014-09-13 19:46:37 +0200

    Pickpocket: basic version
    Built in unlimited pickpocketing while collisioning limiter

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -6,31 +6,32 @@ public class PlayerControl : MonoBehaviour
 {
     public enum PlayerActions
     {
-        PickPocket
+        PickPocket = 0
 
     }
     
     public ControlScheme ControlScheme;
-
-
-    private Vector2 Input;
-
-    private CharacterController cc;
     
+    private CharacterController cc;
+    private PickPocket pp;
+
 	// Use this for initialization
 	void Start () 
     {
         cc = gameObject.GetComponent<CharacterController>();
+        pp = gameObject.GetComponent<PickPocket>();
 
         ControlScheme = ControlScheme.CreateScheme<PlayerActions>();
-
+        ControlScheme.Actions[(int)PlayerActions.PickPocket].Keys.Add(ControlKey.PCKey(KeyCode.Space));
+        ControlScheme.Actions[(int)PlayerActions.PickPocket].Keys.Add(ControlKey.XboxButton(XboxCtrlrInput.XboxButton.A));
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
         ControlScheme.Update();
+
         cc.SetInput(ControlScheme.Horizontal.Value(), ControlScheme.Vertical.Value());
-        Input = new Vector2(ControlScheme.Horizontal.Value(), ControlScheme.Vertical.Value());
+        pp.SetInput(ControlScheme.Actions[(int)PlayerActions.PickPocket].IsPressed());
 	}
 }
