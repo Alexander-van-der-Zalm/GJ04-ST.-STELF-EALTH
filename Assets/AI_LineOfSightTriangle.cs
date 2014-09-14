de81012658fc7d9edfdec310a963c34e9a2a8a10
Alexander van der Zalm <alexandervdzalm@gmail.com>
2014-09-14 21:53:51 +0200

    AI: Sir I need you to move from my LOS

--- Assets/AI_LineOfSightTriangle.cs
+++ Assets/AI_LineOfSightTriangle.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AI_LineOfSightTriangle : MonoBehaviour {
+public class AI_LineOfSightTriangle : MonoBehaviour 
+{
+    public GameObject Pivot;
+    public Vector3 target, pos;
+    public Vector3 dir;
+    public void Update()
+    {
+        RotateTowardsV2(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+    }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public void RotateTowardsV2(Vector2 target)
+    {
+        this.target = target;
+
+        pos = Pivot.transform.position;
+        dir = this.target - pos;
+        dir.z = 0;
+        Quaternion newRot = Quaternion.LookRotation(dir,new Vector3(0,1,0));//pos, target);
+        Pivot.transform.rotation = newRot;
+    }
 }
