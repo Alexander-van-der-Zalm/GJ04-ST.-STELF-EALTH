0153db74b0a6e7a26f18e7b86faaed368b924381
Alexander van der Zalm <alexandervdzalm@gmail.com>
2014-09-14 16:26:44 +0200

    Prefab it up

---
+++ Assets/IAlert.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IAlert : MonoBehaviour 
+{
+    public GameObject AlertSpriteGameObject;
+    [ReadOnly]
+    public bool Alerted;
+    
+    public void Start()
+    {
+        SetAlertedBool(false);
+    }
+
+    public void SetAlerted(float seconds = 20.0f)
+    {
+        StartCoroutine(SetAlertedSimpleCR(seconds));
+    }
+
+    private IEnumerator SetAlertedSimpleCR(float seconds)
+    {
+        SetAlertedBool(true);
+        yield return new WaitForSeconds(seconds);
+        SetAlertedBool(false);
+    }
+	
+    public void StopAlerted()
+    {
+        StopAllCoroutines();
+        SetAlertedBool(false);
+    }
+
+    private void SetAlertedBool(bool alerted)
+    {
+        Alerted = alerted;
+        AlertSpriteGameObject.SetActive(Alerted);
+    }
+}
