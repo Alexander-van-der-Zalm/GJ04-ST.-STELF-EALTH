6d1b8125590b1b7099e4a159ec86299467e44669
Alexander van der Zalm <alexandervdzalm@gmail.com>
2014-10-06 15:58:03 +0200

    Behavior Tree: Basic implementation
    BO00000oooooooom!

---
+++ Assets/Code FrameWork/AI/Simple BT/BT base classes/BT_UINode.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BT_UINode : MonoBehaviour 
+{
+    public BT_Behavior Node;
+    public int X, Y;
+
+    // TODO figure out the rest
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
