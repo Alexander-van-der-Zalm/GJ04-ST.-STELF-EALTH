19d8a032f93871609db89654ed29763f600147cc
Alexander van der Zalm <alexandervdzalm@gmail.com>
2014-10-25 20:38:04 +0200

    Serialization: Generic serializable dictionary

--- Assets/Code/FrameWork/Utility/Custom Types/UDictionary.cs
+++ Assets/Code/FrameWork/Utility/Custom Types/UDictionary.cs
@@ -1,15 +1,155 @@
 ﻿using UnityEngine;
+using System;
+using System.Linq;
 using System.Collections;
+using System.Collections.Generic;
 
-public class UDictionary : MonoBehaviour {
+[Serializable]
+public class UDictionary<K,V> : IDictionary<K,V>, ISerializationCallbackReceiver
+{
+    #region Fields
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Used for serialization
+    [SerializeField]
+    private List<K> keys;
+
+    [SerializeField]
+    private List<V> values;
+
+    // When deserialized
+    [NonSerialized]
+    private Dictionary<K, V> dictionary;
+
+    #endregion
+
+    #region Constructor
+
+    public UDictionary()
+    {
+        dictionary = new Dictionary<K, V>();
+    }
+
+    #endregion
+
+    #region Serialization
+
+    // Deserialize
+    public void OnAfterDeserialize()
+    {
+        dictionary = new Dictionary<K, V>();
+
+        for(int i =0; i < values.Count; i++)
+            dictionary[keys[i]] = values[i];
+
+        //Debug.Log("Deserialized: " + keys.Count + " | " + values.Count);
+    }
+
+    // Serialize
+    public void OnBeforeSerialize()
+    {
+        //Debug.Log("Serialization");
+        if (dictionary == null)
+            dictionary = new Dictionary<K, V>();
+
+        values = dictionary.Values.ToList();
+        keys = dictionary.Keys.ToList();
+    }
+
+    #endregion
+
+    #region Dictionary Implmentation
+
+    public void Add(K key, V value)
+    {
+        dictionary.Add(key, value);
+    }
+
+    public bool ContainsKey(K key)
+    {
+        return dictionary.ContainsKey(key);
+    }
+
+    public ICollection<K> Keys
+    {
+        get { return dictionary.Keys; }
+    }
+
+    public bool Remove(K key)
+    {
+        return dictionary.Remove(key);
+    }
+
+    public bool TryGetValue(K key, out V value)
+    {
+        return dictionary.TryGetValue(key, out value);
+    }
+
+    public ICollection<V> Values
+    {
+        get { return dictionary.Values; }
+    }
+
+    public V this[K key]
+    {
+        get
+        {
+            return dictionary[key];
+        }
+        set
+        {
+            dictionary[key] = value;
+        }
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        return dictionary.GetEnumerator();
+    }
+
+    public void Add(KeyValuePair<K, V> item)
+    {
+        dictionary.Add(item.Key,item.Value);
+    }
+
+    public void Clear()
+    {
+        dictionary.Clear();
+        keys.Clear();
+        values.Clear();
+    }
+
+    public bool Contains(KeyValuePair<K, V> item)
+    {
+        return dictionary.Contains(item);
+    }
+
+    public void CopyTo(KeyValuePair<K, V>[] array, int arrayIndex)
+    {
+        for(int i = 0; i < array.Length; i++)
+        {
+            dictionary[array[i].Key] = array[i].Value;
+        }
+    }
+
+    public int Count
+    {
+        get { return dictionary.Count; }
+    }
+
+    public bool IsReadOnly
+    {
+        get { return false; }
+    }
+
+    public bool Remove(KeyValuePair<K, V> item)
+    {
+        return dictionary.Remove(item.Key);
+    }
+
+    IEnumerator<KeyValuePair<K, V>> IEnumerable<KeyValuePair<K, V>>.GetEnumerator()
+    {
+        return dictionary.GetEnumerator();
+    }
+
+    #endregion
 }
---
+++ Assets/TestMono.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class TestMono : MonoBehaviour//, ISerializationCallbackReceiver
+{
+    //public TestDictionary2 Dictionary;
+    public StringFloatUDictionary Dictionary;
+
+    //public TestDictionary test;
+
+    //public UDictionary<string, float> Dictionary;
+
+	// Use this for initialization
+	void OnEnable () 
+    {
+        //if (test == null)
+        //{
+        //    test = ScriptableObject.CreateInstance<TestDictionary>();
+        //    //test.Init();
+        //}
+	}
+
+    //public void OnAfterDeserialize()
+    //{
+    //    //Dictionary.OnAfterDeserialize();
+    //}
+
+    //public void OnBeforeSerialize()
+    //{
+    //    //Dictionary.OnBeforeSerialize();
+    //}
+}
