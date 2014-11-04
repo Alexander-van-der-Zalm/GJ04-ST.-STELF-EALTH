34ab7db6ee14f85b5843e3da45cd15219e89001c
Alexander van der Zalm <alexandervdzalm@gmail.com>
2014-11-04 23:49:55 +0100

    Agent: editor upgrade

--- Assets/Code/FrameWork/AI/Editor/AI_AgentEditor.cs
+++ Assets/Code/FrameWork/AI/Editor/AI_AgentEditor.cs
@@ -14,29 +14,64 @@ public class AI_AgentEditor : EditorPlus
 
         if (GUI.changed)
         { // Force a selection change
-            if(BTNodeWindowEditor.IsOpen)
+            if (BTNodeWindowEditor.IsOpen)
                 BTNodeWindowEditor.Instance.SelectionChange();
         }
 
-        EditorGUILayout.BeginHorizontal();
-        
-        if (GUILayout.Button("Show Tree"))
-        {
-            BTNodeWindowEditor.ShowWindow();
-            BTNodeWindowEditor.Instance.SelectionChange();
-        }
-            
-        if (GUILayout.Button("Hide Tree"))
-        {
-            BTNodeWindowEditor.HideWindow();
-        }
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Tree Controls:");
 
+        #region Show Hide Tree
+
+        EditorGUILayout.BeginHorizontal();
+        {
+            if (GUILayout.Button("Show Tree"))
+            {
+                BTNodeWindowEditor.ShowWindow();
+                BTNodeWindowEditor.Instance.SelectionChange();
+            }
+
+            if (GUILayout.Button("Hide Tree"))
+            {
+                BTNodeWindowEditor.HideWindow();
+            }
+        }
         EditorGUILayout.EndHorizontal();
 
-        if (GUILayout.Button("Tick Tree"))
+        #endregion
+
+        #region Stop Start Tree
+
+        EditorGUILayout.BeginHorizontal();
         {
-            agent.TreeTick();
-            BTNodeWindowEditor.Instance.Repaint();
+            // Stop start tree
+            if (agent.TreeRunning)
+            {
+                if (GUILayout.Button("Stop Tree"))
+                {
+                    //agent.TreeTick();
+                    //BTNodeWindowEditor.Instance.Repaint();
+                }
+            }
+            else
+            {
+                if (GUILayout.Button("Start Tree"))
+                {
+                    //agent.TreeTick();
+                    //BTNodeWindowEditor.Instance.Repaint();
+                }
+            }
+
+            // Manual tick
+            if (GUILayout.Button("Tick Tree"))
+            {
+                // Pause tree?
+                agent.TreeTick();
+                BTNodeWindowEditor.Instance.Repaint();
+            }
         }
+        EditorGUILayout.EndHorizontal();
+
+        #endregion
     }
 }
