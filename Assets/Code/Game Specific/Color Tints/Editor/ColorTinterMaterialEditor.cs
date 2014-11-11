a4865144ff89b8eb1edb5e80e0eb85ff91d3fa32
Alexander van der Zalm <alexandervdzalm@gmail.com>
2014-11-11 13:46:02 +0100

    MaterialEditor: Started

--- Assets/Code/Game Specific/Color Tints/Editor/ColorTinterMaterialEditor.cs
+++ Assets/Code/Game Specific/Color Tints/Editor/ColorTinterMaterialEditor.cs
@@ -6,7 +6,14 @@ public class ColorTinterMaterialEditor : MaterialEditor
 {
     public override void OnInspectorGUI () 
     {
-        EditorGUILayout.LabelField("TEST");
-        base.OnInspectorGUI ();
+        if(EditorPlus.SavedFoldoutShared("DefaultInspector",-1,"Inspector"))
+            base.OnInspectorGUI ();
+
+        // Show Sprite
+        // Do a correct preview
+
+        // Foldout Color Palettes
+        // - Show All color Palettes
+        // Differ between mem and gpu version
     }
 }
